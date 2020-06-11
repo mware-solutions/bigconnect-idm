5586c5492e87286dfc85701896e959b32a9eb28f
dan.chirila <dan.chirila@m-ware.eu>
2020-06-11 15:32:37 +0300

    Moved on mysql connector

--- docker/tools/build-keycloak.sh
+++ docker/tools/build-keycloak.sh
@@ -54,6 +54,12 @@ cd /opt/jboss/keycloak/modules/system/layers/base/org/mariadb/jdbc/main
 curl -L https://repo1.maven.org/maven2/org/mariadb/jdbc/mariadb-java-client/$JDBC_MARIADB_VERSION/mariadb-java-client-$JDBC_MARIADB_VERSION.jar > mariadb-jdbc.jar
 cp /opt/jboss/tools/databases/mariadb/module.xml .
 
+mkdir -p /opt/jboss/keycloak/modules/system/layers/base/com/mysql/jdbc/main
+cd /opt/jboss/keycloak/modules/system/layers/base/com/mysql/jdbc/main
+curl -O https://repo1.maven.org/maven2/mysql/mysql-connector-java/$JDBC_MYSQL_VERSION/mysql-connector-java-$JDBC_MYSQL_VERSION.jar
+cp /opt/jboss/tools/databases/mysql/module.xml .
+sed "s/JDBC_MYSQL_VERSION/$JDBC_MYSQL_VERSION/" /opt/jboss/tools/databases/mysql/module.xml > module.xml
+
 ######################
 # Configure Keycloak #
 ######################
